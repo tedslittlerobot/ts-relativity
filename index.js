3f52138f43ed862ac56a2191798bfe07017631f8
Stefan Horner <stefan.horner@umotif.com>
2024-05-29 16:51:16 +0100

    nicer docs and some code comments

--- index.js
+++ index.js
@@ -7,11 +7,22 @@ const [ sourceDir, destinationDir ] = argv.slice(2);
 
 await replaceFilesInDirectory(destinationDir);
 
+////////////////////////////////////////////////////////////////////////////////
+///       Helpers                                                            ///
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Recursively replace with relative imports in a given directory
+ *
+ * @param  {string} directory The directory to scan in
+ * @param  {Number} depth     A marker to store the depth level we are currently in
+ * @return Promise<void>
+ */
 async function replaceFilesInDirectory(directory, depth = 1) {
   (await readdir(directory, { withFileTypes: true }))
     .forEach(async item => {
       if (item.isDirectory()) {
-        replaceFilesInDirectory(`${directory}/${item.name}`, depth + 1)
+        await replaceFilesInDirectory(`${directory}/${item.name}`, depth + 1)
 
         return;
       }
@@ -22,6 +33,14 @@ async function replaceFilesInDirectory(directory, depth = 1) {
     })
 }
 
+/**
+ * Perform the replacements in a given file
+ *
+ * @param  {string} directory
+ * @param  {string} file
+ * @param  {Number} depth
+ * @return Promise<void>
+ */
 async function performReplacements(directory, file, depth) {
   const path = `${directory}/${file}`;
   const contents = await readFile(path, 'utf-8')
@@ -35,6 +54,12 @@ async function performReplacements(directory, file, depth) {
   )
 }
 
+/**
+ * Generate the relative path
+ *
+ * @param  {Number} depth
+ * @return {string}
+ */
 function relativeEscapeForDepth(depth) {
   if (depth === 1) return './';
 
--- readme.md
+++ readme.md
@@ -31,22 +31,29 @@ import * from '../../../../helpers/baz.js'
 
 ## Usage:
 
+### Via `npx`
+
 Given your TS source files are in `./src`, and you are building to `./dist`:
 
 ```bash
 npx @tedslittlerobot/ts-relativity src dist
 ```
 
-Within package.json scripts:
+### Within `package.json` scripts
+
+Install locally first:
 
 ```bash
 npm i -D @tedslittlerobot/ts-relativity
 ```
 
+Then use its bin name (`ts-relativity`) or alias (`ts-rel`):
+
 ```json
 {
   "scripts": {
-    "build": "tsc && ts-rel src dist"
+    "build": "tsc && ts-relativity src dist", // using full package name
+    "build": "tsc && ts-rel src dist" // or using a shorter alias
   }
 }
 ```
