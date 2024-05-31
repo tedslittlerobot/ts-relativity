317dbf7fd5f57827f445f4fa4022986b438995c2
Stefan Horner <stefan.horner@umotif.com>
2024-05-31 09:23:22 +0100

    add test

--- test.js
+++ test.js
@@ -1,67 +1,12 @@
-#! /usr/bin/env node
+import test from 'ava';
+import { readFileSync } from 'node:fs';
 
-import { readFile, readdir, writeFile } from 'node:fs/promises';
-import { argv } from 'node:process';
+['top.js', 'foo/depth-one.js', 'foo/bar/depth-two.js']
+  .forEach(path => {
+    test(`Check ${path}`, t => {
+      const actual = readFileSync(`test/run/${path}`).toString();
+      const expected = readFileSync(`test/target/${path}`).toString();
 
-const [ sourceDir, destinationDir ] = argv.slice(2);
-
-await replaceFilesInDirectory(destinationDir);
-
-////////////////////////////////////////////////////////////////////////////////
-///       Helpers                                                            ///
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * Recursively replace with relative imports in a given directory
- *
- * @param  {string} directory The directory to scan in
- * @param  {Number} depth     A marker to store the depth level we are currently in
- * @return Promise<void>
- */
-async function replaceFilesInDirectory(directory, depth = 1) {
-  (await readdir(directory, { withFileTypes: true }))
-    .forEach(async item => {
-      if (item.isDirectory()) {
-        await replaceFilesInDirectory(`${directory}/${item.name}`, depth + 1)
-
-        return;
-      }
-
-      if (item.name.endsWith('.js')) {
-        await performReplacements(directory, item.name, depth);
-      }
-    })
-}
-
-/**
- * Perform the replacements in a given file
- *
- * @param  {string} directory
- * @param  {string} file
- * @param  {Number} depth
- * @return Promise<void>
- */
-async function performReplacements(directory, file, depth) {
-  const path = `${directory}/${file}`;
-  const contents = await readFile(path, 'utf-8')
-
-  await writeFile(
-    path,
-    contents
-      .replaceAll(`from "${sourceDir}/`, `from "${relativeEscapeForDepth(depth)}`)
-      .replaceAll(`from '${sourceDir}/`, `from '${relativeEscapeForDepth(depth)}`),
-    'utf-8'
-  )
-}
-
-/**
- * Generate the relative path
- *
- * @param  {Number} depth
- * @return {string}
- */
-function relativeEscapeForDepth(depth) {
-  if (depth === 1) return './';
-
-  return '../'.repeat(depth - 1);
-}
+      t.is(actual, expected);
+    });
+  });
